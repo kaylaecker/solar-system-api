c347f86778865746abf29a2fa0d1a32d3279a425
janicelichtman <jalichtman@gmail.com>
2021-06-04 12:44:05 -0700

    added GET all planets

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,33 @@
-from flask import request, Blueprint, make_response
+from flask import request, Blueprint, make_response, jsonify
 from app import db
 from app.models.planet import Planet
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/solar_system")
 
-@planets_bp.route("/planets", methods=["POST"])
+@planets_bp.route("/planets", methods=["GET", "POST"])
 def handle_planets():
-    request_body = request.get_json()
-    new_planet = Planet(name=request_body["name"],
-                    description=request_body["description"],
-                    position_from_sun=request_body["position_from_sun"])
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "position": planet.position_from_sun
+            })
+        return jsonify(planets_response)
+    
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["name"],
+                        description=request_body["description"],
+                        position_from_sun=request_body["position_from_sun"])
 
-    db.session.add(new_planet)
-    db.session.commit()
+        db.session.add(new_planet)
+        db.session.commit()
 
-    return make_response(f"Planet {new_planet.name} successfully created", 201)
-
-# @planets_bp.route("/planets", methods=["GET"])
-# def endpoint_name():
-#     response_body = {
-#     "id": "?????",
-#     "name": "Pluto",
-#     "description": "The best planet, despite Neil.",
-#     "position_from_sun": 9
-#     }
-#     return response_body
+        return make_response(f"Planet {new_planet.name} successfully created", 201)
 
 
 
